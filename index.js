05214a820167ebd19b87b067558924567b7bacd0
code-sign <30948029+code-sign@users.noreply.github.com>
2018-06-10 13:56:59 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
+var swiper = new Swiper('.swiper-container.type1', {
+    // effect: 'coverflow',
+    freeMode: true,
+    grabCursor: true,
+    // centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 180,
+        modifier: 1,
+        slideShadows : true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    loop:true,
+    spaceBetween: 10,
+});
+
+
+
+$('.img_box').on('click', function(){
+    var _thisBox = $(this);
+    var thisImg = $(this).find('.img');
+    var allImgBox = $('.img_box .img');
+
+    if($(this).hasClass('active')){
+        TweenMax.to(thisImg, .5, {width:'150px',height:'150px'} );
+        setTimeout(function(){
+            _thisBox.removeClass('active');
+        },500);
+        // $(this).removeClass('active');
+        
+    } else {
+        TweenMax.to(allImgBox, .5, {width:'150px',height:'150px'} );
+        TweenMax.to(thisImg, .5, {width:'100%',height:'300px'} );
+        $(this).addClass('active').siblings('a').removeClass('active');
+        
+    }
+});
+
+
+
 var navLi = $('.nav_mn > li');
+
 
 $('#menuOpen').click(function(){
     TweenMax.staggerFrom(navLi, 0.5, {
